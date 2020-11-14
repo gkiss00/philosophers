125f85c6cdbb061c475a5dd0775aeb08d5aa543e
Gautier Kiss <gkgkgk395@gmail.com>
2020-11-14 13:59:25 +0100

    philo_one normed

--- philo_one/actions.c
+++ philo_one/actions.c
@@ -1,54 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   actions.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gkiss <gkiss@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/29 12:24:49 by cochapel          #+#    #+#             */
+/*   Updated: 2020/11/14 13:52:27 by gkiss            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "philo_one.h"
 
-extern int time_to_eat;
-extern int time_to_sleep;
-extern int end;
+extern int	g_time_to_eat;
+extern int	g_time_to_sleep;
+extern int	g_end;
 
-void        ft_wait_start(s_philosof  *philo)
+void		ft_wait_start(t_philosof *philo)
 {
-    long int     actual;
+	long int	actual;
 
-    actual = get_time();
-    while(actual < philo->start)
-    {
-        actual = get_time();
-    }
+	actual = get_time();
+	while (actual < philo->start)
+	{
+		actual = get_time();
+	}
 }
 
-void        ft_eat(s_philosof  *philo)
+void		ft_eat(t_philosof *philo)
 {
-    if (philo->fork_left_id < philo->fork_right_id)
-    {
-        pthread_mutex_lock(philo->fork_left);
-        put_message(philo, " taken his left fork\n");
-        pthread_mutex_lock(philo->fork_right);
-        put_message(philo, " taken his right fork\n");
-    }
-    else
-    {
-        pthread_mutex_lock(philo->fork_right);
-        put_message(philo, " taken his right fork\n");
-        pthread_mutex_lock(philo->fork_left);
-        put_message(philo, " taken his left fork\n");
-    }
-    put_message(philo, " is eating\n");
-    philo->last_meal = get_time();
-    usleep(time_to_eat * 1000);
-    philo->nb_meal += 1;
-    philo->last_meal = get_time();
+	if (philo->fork_left_id < philo->fork_right_id)
+	{
+		pthread_mutex_lock(philo->fork_left);
+		put_message(philo, " taken his left fork\n");
+		pthread_mutex_lock(philo->fork_right);
+		put_message(philo, " taken his right fork\n");
+	}
+	else
+	{
+		pthread_mutex_lock(philo->fork_right);
+		put_message(philo, " taken his right fork\n");
+		pthread_mutex_lock(philo->fork_left);
+		put_message(philo, " taken his left fork\n");
+	}
+	put_message(philo, " is eating\n");
+	philo->last_meal = get_time();
+	usleep(g_time_to_eat * 1000);
+	philo->nb_meal += 1;
+	philo->last_meal = get_time();
 }
 
-void        ft_sleep(s_philosof  *philo)
+void		ft_sleep(t_philosof *philo)
 {
-    if (*philo->alive == 1 && end == 0)
-    {
-        put_message(philo, " is sleeping\n");
-        usleep(time_to_sleep * 1000);
-    }
+	if (*philo->alive == 1 && g_end == 0)
+	{
+		put_message(philo, " is sleeping\n");
+		usleep(g_time_to_sleep * 1000);
+	}
 }
 
-void        ft_think(s_philosof  *philo)
+void		ft_think(t_philosof *philo)
 {
-    if (*philo->alive == 1 && end == 0)
-        put_message(philo, " is thinking\n");
+	if (*philo->alive == 1 && g_end == 0)
+		put_message(philo, " is thinking\n");
 }
--- philo_one/ft_philo.c
+++ philo_one/ft_philo.c
@@ -1,124 +1,136 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_philo.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gkiss <gkiss@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/29 12:24:49 by cochapel          #+#    #+#             */
+/*   Updated: 2020/11/14 13:49:31 by gkiss            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "philo_one.h"
 
-extern int  n_p;
-extern int  time_to_die;
-extern int  time_to_eat;
-extern int  time_to_sleep;
-extern int  me;
-int         alive = 1;
-int         end = 0;
+extern int	g_n_p;
+extern int	g_time_to_die;
+extern int	g_time_to_eat;
+extern int	g_time_to_sleep;
+extern int	g_me;
+int			g_alive = 1;
+int			g_end = 0;
 
-static void    end_simulation(s_philosof philo[n_p], int nb, int i)
+static void		end_simulation(t_philosof philo[g_n_p], int nb, int i)
 {
-    while(*philo[0].alive == 1 && end == 0)
-    {
-        if (me >= 0)
-        {
-            nb = 0;
-            i = 0;
-            while (i < n_p)
-            {
-                if (philo[i].nb_meal >= me)
-                    ++nb;
-                ++i;
-            }
-            if (nb == n_p)
-            {
-                pthread_mutex_lock(philo->write);
-                ft_putstr("Tout les philosophes ont mangé ");
-                ft_putnbr(me);
-                ft_putstr(" fois. La simulation prend fin.\n");
-                end = 1;
-                pthread_mutex_unlock(philo->write);
-            }    
-        }
-    }
+	while (*philo[0].alive == 1 && g_end == 0)
+	{
+		if (g_me >= 0)
+		{
+			nb = 0;
+			i = 0;
+			while (i < g_n_p)
+			{
+				if (philo[i].nb_meal >= g_me)
+					++nb;
+				++i;
+			}
+			if (nb == g_n_p)
+			{
+				pthread_mutex_lock(philo->write);
+				ft_putstr("Tout les philosophes ont mangé ");
+				ft_putnbr(g_me);
+				ft_putstr(" fois. La simulation prend fin.\n");
+				g_end = 1;
+				pthread_mutex_unlock(philo->write);
+			}
+		}
+	}
 }
 
-static void     *death_controle(void *arg)
+static void		*death_controle(void *arg)
 {
-    s_philosof      *philo;
+	t_philosof	*philo;
 
-    philo = (s_philosof*)arg;
-    ft_wait_start(philo);
-    while(*philo->alive == 1 && end == 0 && me != 0)
-    {
-        if (get_time_dif_l(philo->last_meal) / 1000 > time_to_die)
-        {
-            put_message_end(philo, " just died\n");
-            *philo->alive = 0;
-        }
-    }
-    return (NULL);
+	philo = (t_philosof*)arg;
+	ft_wait_start(philo);
+	while (*philo->alive == 1 && g_end == 0 && g_me != 0)
+	{
+		if (get_time_dif_l(philo->last_meal) / 1000 > g_time_to_die)
+		{
+			put_message_end(philo, " just died\n");
+			*philo->alive = 0;
+		}
+	}
+	return (NULL);
 }
 
-static void     *start(void *arg)
+static void		*start(void *arg)
 {
-    s_philosof      *philo;
+	t_philosof	*philo;
 
-    philo = (s_philosof*)arg;
-    ft_wait_start(philo);
-    if (philo->id % 2 == 0)
-        usleep((time_to_eat + time_to_sleep) * 1000 / 2);
-    while(*philo->alive == 1 && end == 0 && me != 0)
-    {
-        ft_eat(philo);
-        pthread_mutex_unlock(philo->fork_left);
-        pthread_mutex_unlock(philo->fork_right);
-        ft_sleep(philo);
-        ft_think(philo);
-        if (alive == 1 && end == 0 && time_to_sleep == 0)
-            usleep(time_to_eat * 1000 / 2);
-    }
-    return (NULL);
+	philo = (t_philosof*)arg;
+	ft_wait_start(philo);
+	if (philo->id % 2 == 0)
+		usleep((g_time_to_eat + g_time_to_sleep) * 1000 / 2);
+	while (*philo->alive == 1 && g_end == 0 && g_me != 0)
+	{
+		ft_eat(philo);
+		pthread_mutex_unlock(philo->fork_left);
+		pthread_mutex_unlock(philo->fork_right);
+		ft_sleep(philo);
+		ft_think(philo);
+		if (g_alive == 1 && g_end == 0 && g_time_to_sleep == 0)
+			usleep(g_time_to_eat * 1000 / 2);
+	}
+	return (NULL);
 }
 
-static void    init_phil(s_philosof philo[n_p], pthread_mutex_t fork[n_p])
+static void		init_phil(t_philosof philo[g_n_p], pthread_mutex_t fork[g_n_p])
 {
-    int             i;
-    long int        start;
+	int			i;
+	long int	start;
 
-    i = 0;
-    start = get_time() + 1000000;
-    while (i < n_p)
-    {
-        philo[i].id = i + 1;
-        philo[i].alive = &alive;
-        philo[i].nb_meal = 0;
-        philo[i].last_meal = start;
-        philo[i].fork_left_id = i;
-        philo[i].fork_right_id = (i + 1) % n_p;
-        philo[i].fork_left = &fork[i];
-        philo[i].fork_right = &fork[(i + 1) % n_p];
-        philo[i].write = &fork[n_p];
-        philo[i].start = start;
-        ++i;
-    }
+	i = 0;
+	start = get_time() + 1000000;
+	while (i < g_n_p)
+	{
+		philo[i].id = i + 1;
+		philo[i].alive = &g_alive;
+		philo[i].nb_meal = 0;
+		philo[i].last_meal = start;
+		philo[i].fork_left_id = i;
+		philo[i].fork_right_id = (i + 1) % g_n_p;
+		philo[i].fork_left = &fork[i];
+		philo[i].fork_right = &fork[(i + 1) % g_n_p];
+		philo[i].write = &fork[g_n_p];
+		philo[i].start = start;
+		++i;
+	}
 }
 
-void            begin_simulation()
+void			begin_simulation(void)
 {
-    int             i;
-    s_philosof      philo[n_p];
-    pthread_t       phil[n_p];
-    pthread_t       death[n_p];
-    pthread_mutex_t fork[n_p + 1];
+	int				i;
+	t_philosof		philo[g_n_p];
+	pthread_t		phil[g_n_p];
+	pthread_t		death[g_n_p];
+	pthread_mutex_t	fork[g_n_p + 1];
 
-    i = -1;
-    while (++i < n_p + 1)
-        pthread_mutex_init(&fork[i], NULL);
-    init_phil(philo, fork);
-    i = -1;
-    while (++i < n_p)
-    {
-        pthread_create(&phil[i], NULL, start, (void*)&philo[i]);
-        pthread_create(&death[i], NULL, death_controle, (void*)&philo[i]);
-    }
-    end_simulation(philo, 0, 0);
-    i = -1;
-    while (++i < n_p)
-        pthread_join(phil[i], NULL);
-    i = -1;
-    while (++i < n_p + 1)
-        pthread_mutex_destroy(&fork[i]);
+	i = -1;
+	while (++i < g_n_p + 1)
+		pthread_mutex_init(&fork[i], NULL);
+	init_phil(philo, fork);
+	i = -1;
+	while (++i < g_n_p)
+	{
+		pthread_create(&phil[i], NULL, start, (void*)&philo[i]);
+		pthread_create(&death[i], NULL, death_controle, (void*)&philo[i]);
+	}
+	end_simulation(philo, 0, 0);
+	i = -1;
+	while (++i < g_n_p)
+		pthread_join(phil[i], NULL);
+	i = -1;
+	while (++i < g_n_p + 1)
+		pthread_mutex_destroy(&fork[i]);
 }
